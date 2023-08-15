9c035a84f568c7dfcd41879fc4c9937dd412ee6d
Paulo Carvalho <5382734+paulo-carvalho@users.noreply.github.com>
2023-08-15 18:46:28 -0300

    install react

--- index.js
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client';
-
-// Limpar o conteúdo HTML existente
-document.body.innerHTML = '<div id="app"></div>';
-
-// Em vez disso, renderize seu componente React
-const root = createRoot(document.getElementById('app'));
-root.render(<h1>Hello, world</h1>);
